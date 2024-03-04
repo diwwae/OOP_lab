5914df1148bcfa72d5ce1cb793246c2fa1946a18
diwwae <dinababintseva@gmail.com>
2024-03-04 13:43:20 +0300

    adding smart ptrs

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 #include "Rectangle.h"
 #include "Trapezoid.h"
 #include "Square.h"
+#include ".h"
+#include <memory>
 
 int main(){
+    std::vector<Point<int>> initialPoints = {Point(1,2),Point(4,2),Point(4,0),Point(1,0)};
+
+    std::shared_ptr<Figure<int>> fig = std::make_shared<Figure<int>>(); 
+    std::shared_ptr<Rectangle<std::vector<Point<int>>>> rec = std::make_shared<Rectangle<std::vector<Point<int>>>>(initialPoints); 
+
+    // rec->Rectangle({Point(1,2),Point(4,2),Point(4,0),Point(1,0)});
+
+    rec->calculateSquare();
+
     // Rectangle rectangle({Point(1,2),Point(4,2),Point(4,0),Point(1,0)}); 
     // Trapezoid trapezoid = Trapezoid::createFromPoints({Point(0,0),Point(1,2),Point(3,2),Point(4,0)});
     // Figure fig({Point(1,2)});
